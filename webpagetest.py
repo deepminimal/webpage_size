d41bab6d65e738a1b72c3de444e5fe7714745301
deepminimal <voffko91@gmail.com>
2019-09-30 18:21:09 +0300

    Update webpagetest.py

--- webpagetest.py
+++ webpagetest.py
@@ -46,6 +46,9 @@ count = 0
 
 for entry in browser.get_log('performance'):
   if "Network.dataReceived" in str(entry):
+    counter += 1
+    if (counter == 1):
+      print(entry)
     r = re.search(r'encodedDataLength\":(.*?),', str(entry))
     r2 = re.search(r'dataLength\":(.*?),', str(entry))
     total_bytes.append(int(r.group(1)))
@@ -57,6 +60,9 @@ newtwork_logs = []
 newtwork_logs = browser.execute_script("var network = performance.getEntries() || {}; return network;")
 for entry2 in newtwork_logs:
   if "transferSize" in str(entry2):
+    count += 1
+    if (count == 1):
+      print(entry2)
     r3 = re.search(r"encodedBodySize\':(.*?),", str(entry2))
     total_bytes3.append(int(r3.group(1)))
 print("encodedBodySize: %s", str(sum(total_bytes3)))
