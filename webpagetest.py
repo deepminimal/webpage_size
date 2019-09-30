eee762aa42644537424c8ae6d33b753079004a4d
deepminimal <voffko91@gmail.com>
2019-09-30 17:24:27 +0300

    Update webpagetest.py

--- webpagetest.py
+++ webpagetest.py
@@ -40,8 +40,12 @@ print(browser.execute_script("return document.body.scrollHeight"))
 WebDriverWait(browser, 60).until(lambda driver: driver.execute_script("return document.readyState == 'complete'"))
 print("end WebDriverWait")
 total_bytes = []
+count = 0
 for entry in browser.get_log('performance'):
         if "Network.dataReceived" in str(entry):
+            counter += 1
+            if (counter == 1):
+              print(str(entry))
             r = re.search(r'encodedDataLength\":(.*?),', str(entry))
             total_bytes.append(int(r.group(1)))
             mb = round((float(sum(total_bytes) / 1000) / 1000), 2)
