2da4e354477429a1871a0d16eb0432a84c1e1b14
deepminimal <voffko91@gmail.com>
2019-09-30 12:34:02 +0300

    Update webpagetest.py

--- webpagetest.py
+++ webpagetest.py
@@ -15,10 +15,12 @@ d['goog:loggingPrefs'] = { 'performance':'ALL' }
 
 browser = webdriver.Chrome(desired_capabilities=d, options=options)
 browser.get('https://ostin.com')
+
 def page_has_loaded(self):
-    self.log.info("Checking if %s page is loaded.", str(self.driver.current_url))
-    page_state = self.driver.execute_script('return document.readyState;')
+    print("Checking if %s page is loaded.", str(self.browser.current_url))
+    page_state = self.browser.execute_script('return document.readyState;')
     return page_state == 'complete'
+
 WebDriverWait(browser, 50).until(page_has_loaded("https://ostin.com"))
 #logs = browser.execute('getLog', {'type': 'performance'})['value']
 #re_encdatalen = re.compile(r'.*encodedDataLength":(-?[0-9]+),.*$')
