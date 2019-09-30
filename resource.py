f86437d71714c3d085e0ca6f8205399367cb5976
deepminimal <voffko91@gmail.com>
2019-09-30 14:03:57 +0300

    Create resource.py

---
+++ resource.py
@@ -0,0 +1,37 @@
+from browsermobproxy import Server
+from selenium import webdriver
+
+# Purpose of this script: List all resources (URLs) that
+# Chrome downloads when visiting some page.
+
+### OPTIONS ###
+url = "https://ostin.com"
+chromedriver_location = "./chromedriver" # Path containing the chromedriver
+browsermobproxy_location = "/opt/browsermob-proxy-2.1.4/bin/browsermob-proxy" # location of the browsermob-proxy binary file (that starts a server)
+chrome_location = "/usr/bin/x-www-browser"
+###############
+
+# Start browsermob proxy
+server = Server(browsermobproxy_location)
+server.start()
+proxy = server.create_proxy()
+
+# Setup Chrome webdriver - note: does not seem to work with headless On
+options = webdriver.ChromeOptions()
+options.binary_location = chrome_location
+# Setup proxy to point to our browsermob so that it can track requests
+options.add_argument('--proxy-server=%s' % proxy.proxy)
+driver = webdriver.Chrome(chromedriver_location, chrome_options=options)
+
+# Now load some page
+proxy.new_har("Example")
+driver.get(url)
+
+# Print all URLs that were requested
+entries = proxy.har['log']["entries"]
+for entry in entries:
+    if 'request' in entry.keys():
+        print entry['request']['url']
+
+server.stop()
+driver.quit()
