3bb2cdef778ad830932baf892bb47760b43ed283
deepminimal <voffko91@gmail.com>
2019-09-30 10:59:02 +0300

    Update webpagetest.py

--- webpagetest.py
+++ webpagetest.py
@@ -1,13 +1,16 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import chromedriver_binary
+
 options = webdriver.ChromeOptions()
 options.add_argument('--no-sandbox')
 options.add_argument('--headless')
 options.add_argument('--disable-dev-shm-usage')
-capbs = webdriver.DesiredCapabilities.CHROME.copy()
-capbs.update({'goog:loggingPrefs': {'performance': 'ALL'}, 'detach': False})
-browser = webdriver.Chrome(capbs, options=options)
+d = DesiredCapabilities.CHROME
+d['goog:loggingPrefs'] = { 'performance':'ALL', 'detach': False }
+
+
+browser = webdriver.Chrome(desired_capabilities=d, options=options)
 browser.get('https://automatetheboringstuff.com')
 logs = browser.execute('getLog', {'type': 'performance'})['value']
 print(logs)
