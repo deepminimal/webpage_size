9202dc4a1a35af7a6bfef9f11265f8b299d7802e
deepminimal <voffko91@gmail.com>
2019-09-30 11:52:00 +0300

    Update webpagetest.py

--- webpagetest.py
+++ webpagetest.py
@@ -23,7 +23,8 @@ loading_finished = [l['message'] for l in logs if
 #lf_enc_data_len = [int(re_encdatalen.match(m)[1]) for m in loading_finished]
 lf_enc_data_len = 0
 for m in loading_finished:
-  lf_enc_data_len += int(re_encdatalen.match(m)
+  lf_enc_data_len += int(re_encdatalen.match(m))
 #lf_enc_data_len_sum = sum(lf_enc_data_len)
+
 print(str(lf_enc_data_len))
 browser.close()
