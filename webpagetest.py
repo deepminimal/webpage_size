1e3d462fb2a2d3f29935059fad57e446113106a8
deepminimal <voffko91@gmail.com>
2019-09-30 11:02:37 +0300

    Update webpagetest.py

--- webpagetest.py
+++ webpagetest.py
@@ -14,5 +14,11 @@ d['goog:loggingPrefs'] = { 'performance':'ALL' }
 browser = webdriver.Chrome(desired_capabilities=d, options=options)
 browser.get('https://automatetheboringstuff.com')
 logs = browser.execute('getLog', {'type': 'performance'})['value']
-print(logs)
+re_encdatalen = re.compile(r'^.*encodedDataLength":(-?[0-9]+),.*$')
+  loading_finished = [l['message'] for l in logs if
+                        'INFO' == l['level'] and 'Network.loadingFinished' in l['message']]
+  lf_enc_data_len = [int(re_encdatalen.match(m)[1]) for m in loading_finished]
+lf_enc_data_len_sum = sum(lf_enc_data_len)
+
+print(str(lf_enc_data_len_sum))
 browser.close()
