56a5925aa8936acc85a2a465cea5d0c6cda37245
deepminimal <voffko91@gmail.com>
2019-09-30 16:28:09 +0300

    Update webpagetest.py

--- webpagetest.py
+++ webpagetest.py
@@ -1,4 +1,5 @@
 import re
+import json
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
