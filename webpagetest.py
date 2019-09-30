7f51d858e03de54a5f7fd89b80a8a27433f64fa8
deepminimal <voffko91@gmail.com>
2019-09-30 14:42:48 +0300

    Update webpagetest.py

--- webpagetest.py
+++ webpagetest.py
@@ -29,4 +29,6 @@ for entry in browser.get_log('performance'):
             total_bytes.append(int(r.group(1)))
             mb = round((float(sum(total_bytes) / 1000) / 1000), 2)
 print(str(sum(total_bytes)))
+browser.save_screenshot("screenshot.png")
+
 browser.close()
