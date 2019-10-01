0f1f79cefdef4dabc14e69d7319ab0deb099f0d6
deepminimal <voffko91@gmail.com>
2019-10-01 11:40:41 +0300

    Update webpagetest.py

--- webpagetest.py
+++ webpagetest.py
@@ -31,8 +31,7 @@ for entry in newtwork_logs:
     total_bytes.append(int(r.group(1)))
 print("transferSize: ", str(sum(total_bytes)))
 total_bytes = []
-browser_preformance_log = json.dumps(browser.get_log('performance'))
-print(json.loads(browser_preformance_log))
+browser_preformance_log = browser.get_log('performance')
 for entry in browser_preformance_log:
         if "Network.loadingFinished" in str(entry):
             r = re.search(r'encodedDataLength\":(.*?),', str(entry))
@@ -40,8 +39,8 @@ for entry in browser_preformance_log:
 print("encodedDataLength: ", str(sum(total_bytes)))
 with open('/usr/share/zabbix/newtwork_logs.json', 'w') as outfile:
     json.dump(newtwork_logs, outfile)
-#with open('/usr/share/zabbix/browser_preformance_log.json', 'w') as outfile:
-#    outfile.write(str(browser_preformance_log))
+with open('/usr/share/zabbix/browser_preformance_log.json', 'w') as outfile:
+    outfile.write(browser_preformance_log)
 #browser.save_screenshot("/usr/share/zabbix/screenshot.png")
 #browser.find_element_by_tag_name('body').screenshot("/usr/share/zabbix/screenshot2.png")
 browser.close()
