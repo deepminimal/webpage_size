d94d0e5c81a5add50740155fab3378ddb7b293a0
deepminimal <voffko91@gmail.com>
2019-10-01 10:48:20 +0300

    Update webpagetest.py

--- webpagetest.py
+++ webpagetest.py
@@ -40,7 +40,7 @@ print("encodedDataLength: ", str(sum(total_bytes)))
 with open('/usr/share/zabbix/newtwork_logs.json', 'w') as outfile:
     json.dump(newtwork_logs, outfile)
 with open('/usr/share/zabbix/browser_preformance_log.json', 'w') as outfile:
-    json.load(browser_preformance_log, outfile)
+    json.loads(browser_preformance_log, outfile)
 #browser.save_screenshot("/usr/share/zabbix/screenshot.png")
 #browser.find_element_by_tag_name('body').screenshot("/usr/share/zabbix/screenshot2.png")
 browser.close()
