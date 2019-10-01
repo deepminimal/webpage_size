411d17208c8000b3f1964f1e58e067b55259a64f
deepminimal <voffko91@gmail.com>
2019-10-01 18:08:28 +0300

    Update webpagetest.py

--- webpagetest.py
+++ webpagetest.py
@@ -6,6 +6,7 @@ from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 import chromedriver_binary
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.common.by import By
 
 print("start new")
 options = webdriver.ChromeOptions()
