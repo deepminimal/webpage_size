696a2ae1f4a69e27dcf4ba47cb7b2408534b96f8
deepminimal <voffko91@gmail.com>
2019-10-02 14:05:56 +0300

    Update webpagetest.py

--- webpagetest.py
+++ webpagetest.py
@@ -30,7 +30,7 @@ browser.execute_script("window.scrollTo(0, 3800);")
 try:
     element_present = EC.presence_of_element_located((By.CLASS_NAME, 'o-footer-contacts-social__container'))
     WebDriverWait(browser, 30).until(element_present)
-
+    element_present.click()
 except:
     print "Timed out waiting for page to load"
 
