d48e0e8469e3472e3badec510ad41e9d8854816a
deepminimal <voffko91@gmail.com>
2019-10-03 15:27:19 +0300

    Update proxy.py

--- proxy.py
+++ proxy.py
@@ -25,8 +25,6 @@ driver.get("https://ostin.com/")
 S = lambda X: driver.execute_script('return document.body.parentNode.scroll'+X)
 total_bytes = []
 
-string = driver.execute_script("fetch('http://elastic-1.productgateost.marathon.mesos.sportmaster.ru:9200/_cluster/health').then(function(response) {return response.json();}).then(function(data) {return data;}).catch(function() {return 'booooooo';});")
-print(string)
 result = json.dumps(proxy.har, ensure_ascii=True, indent=2)
 
 end = 0
