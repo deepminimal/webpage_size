2b6125170570a68c943a9c917f93ec65789d98a5
deepminimal <voffko91@gmail.com>
2019-10-03 18:22:55 +0300

    Update proxy.py

--- proxy.py
+++ proxy.py
@@ -33,7 +33,7 @@ for entry in proxy.har['log']['entries']:
   mimeType.append(entry['response']['content']['mimeType'])
   bodySize.append(int(entry['response']['bodySize']))
 print(list(set(mimeType)))
-d = {}
+d = хъ
 for entry in proxy.har['log']['entries']:
   for i in range(1, len(mimeType)):
     if (mimeType[i] == entry['response']['content']['mimeType']):
