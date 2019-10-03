f8c58c7f47df78e20d8f8ecadd4bde6767da772b
deepminimal <voffko91@gmail.com>
2019-10-03 16:20:22 +0300

    Update proxy.py

--- proxy.py
+++ proxy.py
@@ -27,7 +27,7 @@ WebDriverWait(driver, 60).until(lambda driver: driver.execute_script("return doc
 S = lambda X: driver.execute_script('return document.body.parentNode.scroll'+X)
 driver.set_window_size(S('Width'),S('Height')) # May need manual adjustment
 result = json.dumps(proxy.har, ensure_ascii=True, indent=2)
-print(type(result))
+print(type(proxy.har))
 r = re.findall(r'bodySize\":(.*?),', str(result))
 var =0
 for num in r:
