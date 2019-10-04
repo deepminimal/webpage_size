5ef480cfa825ba6fb628e2552b0d8d9294a7b145
deepminimal <voffko91@gmail.com>
2019-10-04 13:52:43 +0300

    Update proxy.py

--- proxy.py
+++ proxy.py
@@ -59,7 +59,8 @@ print("bodySize: ", str(sum(bodySize)))
 
 
 with open('/usr/share/zabbix/result.json', 'w') as outfile:
-  outfile.write(example)
+  outfile.write(json.dumps(example))
+  
 driver.find_element_by_tag_name('body').screenshot('/usr/share/zabbix/screenshot.png')
 server.stop()    
 driver.quit()
