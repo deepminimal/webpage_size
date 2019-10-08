0868d3f3d77dacb7051283100e6eba6e2e7395c6
deepminimal <voffko91@gmail.com>
2019-10-08 12:28:27 +0300

    Update api.py

--- api.py
+++ api.py
@@ -59,7 +59,7 @@ class GET_PAGE_SIZE(Resource):
       #      keys[mimeType[i]][entries] = {'bodySize': int(har[entries]['response']['bodySize']),'URL': str(har[entries]['request']['url'])}
       #example['result'] = keys 
       driver.quit()
-      return {'bodySize':str(sum(bodySize)), 'time':str(sum(download_time)), 'startDownloadTime':str(proxy.har['log']['entries'][0]['startedDateTime'], }
+      return {'bodySize':str(sum(bodySize)), 'time':str(sum(download_time)), 'startDownloadTime':str(proxy.har['log']['entries'][0]['startedDateTime'] }
     except Exception as e:
       print("ERROR: %s" % str(e))
 try:        
