c07994d3f99f2dbb169b21d0530c0c3158f9ab5a
deepminimal <voffko91@gmail.com>
2019-10-09 20:04:23 +0300

    Update api.py

--- api.py
+++ api.py
@@ -16,65 +16,14 @@ try:
 except ImportError:
   from StringIO import StringIO as BytesIO
   
-os.popen("pkill -9 java")
-os.popen("pkill -9 brows")
-server = Server("./browsermob-proxy-2.1.4/bin/browsermob-proxy", options={'port': 5100})
-server.start()
+
 app = Flask(__name__)
 api = Api(app)
+
 class GET_PAGE_SIZE(Resource):
   def get(self,URL):
     try:
-      print("start get fed")
-      proxy = server.create_proxy()
-      chromedriver = "./chromedriver"
-      os.environ["webdriver.chrome.driver"] = chromedriver
-      url = urlparse.urlparse(proxy.proxy).path
-      chrome_options = webdriver.ChromeOptions()
-      chrome_options.add_argument('--no-sandbox')
-      chrome_options.add_argument('--headless')
-      chrome_options.add_argument('--disable-dev-shm-usage')
-      chrome_options.add_argument("--proxy-server={0}".format(url))
-      print("driver")
-      driver = webdriver.Chrome(chromedriver,chrome_options =chrome_options)
-      driver.set_window_size(1920, 1080)
-      print("try")
-      try:
-        print("proxy.new_har")
-        proxy.new_har(str(URL),options={'captureHeaders': True, 'captureContent':True, 'captureBinaryContent':True})
-        print("driver.get")
-        driver.get(URL)
-        status_code = proxy.wait_for_traffic_to_stop(100, 20000)
-        
-      except Exception as err:
-        print("ERROR: %s" % str(err))
-      #WebDriverWait(driver, 30).until(lambda driver: driver.execute_script("return document.readyState == 'complete'"))
-      #S = lambda X: driver.execute_script('return document.body.parentNode.scroll'+X)
-      #driver.set_window_size(S('Width'),S('Height'))
-      #har = proxy.har['log']['entries']
-      #mimeType = []
-      bodySize = []
-      download_time = []
-      counter = 0
-      for entry in proxy.har['log']['entries']:
-        counter += 1
-        #mimeType.append(entry['response']['content']['mimeType'])
-        bodySize.append(int(entry['response']['bodySize']))
-        download_time.append(int(entry['time']))
-      #example = defaultdict(dict)
-      #keys = defaultdict(dict)
-      #for i in range(1, len(mimeType)):
-      #  for entries in range(1, len(har)):
-      #    if (mimeType[i] == har[entries]['response']['content']['mimeType']):
-      #      keys[mimeType[i]][entries] = {'bodySize': int(har[entries]['response']['bodySize']),'URL': str(har[entries]['request']['url'])}
-      #example['result'] = keys 
-      print("driver.quit")
-      driver.quit()
-      startDownloadTime = datetime.datetime.strptime(str(proxy.har['log']['entries'][0]['startedDateTime']), '%Y-%m-%dT%H:%M:%S.%fZ')
-      LastStartDownloadTime = datetime.datetime.strptime(str(proxy.har['log']['entries'][counter-1]['startedDateTime']), '%Y-%m-%dT%H:%M:%S.%fZ')
-      proxy.close()
-      print("end")
-      return {'bodySize':str(sum(bodySize)), 'time':str(sum(download_time)), 'LastStartDownloadTime': str(LastStartDownloadTime), 'startDownloadTime': str(startDownloadTime), 'total_download_time': str((LastStartDownloadTime - startDownloadTime).total_seconds())}
+      return "OK"
     except Exception as e:
       print("ERROR: %s" % str(e))
 try:        
